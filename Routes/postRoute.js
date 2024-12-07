6d2131eb2bbdaa578345e7d892dbd921ce6d5b59
24HP5566 24HP5566 <24HP5566@admins-iMac.local>
2024-12-07 11:40:10 +0800

    last

--- Routes/postRoute.js
+++ Routes/postRoute.js
@@ -3,13 +3,11 @@ const {
   createPost,
   getPosts,
   getOnlyOnePost,
-  getOnlyOnePostComments,
 } = require("../Controller/postController");
 const postRoute = Router();
 
 postRoute.post("/createPost", createPost);
 postRoute.get("/getPost", getPosts);
 postRoute.post("/getOnlyOnePost", getOnlyOnePost);
-postRoute.post("/getOnlyOnePostComments", getOnlyOnePostComments);
 
 module.exports = postRoute;
