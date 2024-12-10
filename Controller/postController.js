77898df2edf59a00aa24a809ddc37734c1a8c04b
24HP5566 24HP5566 <24HP5566@admins-iMac.local>
2024-12-10 16:01:42 +0800

    l

--- Controller/postController.js
+++ Controller/postController.js
@@ -53,6 +53,7 @@ const getPostComments = async (req, res) => {
   try {
     const { postId } = req.query;
     const post = await postModel.findOne(postId).populate({
+      select: "comments",
       path: "comments",
       populate: {
         path: "userId",
