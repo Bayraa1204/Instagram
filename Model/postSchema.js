e6600daa54a1571f798d3217c78ec54fd7b9b0a0
24HP5566 24HP5566 <24HP5566@admins-iMac.local>
2025-01-07 16:56:24 +0800

    idk

--- Model/postSchema.js
+++ Model/postSchema.js
@@ -3,7 +3,7 @@ const { Schema, mongoose } = require("mongoose");
 const postSchema = new Schema(
   {
     caption: { type: String, required: true },
-    postImg: { type: String, required: true },
+    postImg: [{ type: String, required: true }],
     userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
     comments: [{ type: mongoose.Types.ObjectId, ref: "Comment" }],
     like: [{ type: mongoose.Types.ObjectId, ref: "User" }],
