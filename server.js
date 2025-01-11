b1f7b5491c02419bc2ec0c4e873cef8198306f35
24HP5566 24HP5566 <24HP5566@admins-iMac.local>
2025-01-11 13:30:19 +0800

    last

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(
   cors({
     origin: [
-      "https://instagram-front-end-f9gz.vercel.app ",
+      "https://instagram-front-end-f9gz.vercel.app",
       "http://localhost:3000",
     ],
     methods: "GET,POST,PUT,DELETE",
